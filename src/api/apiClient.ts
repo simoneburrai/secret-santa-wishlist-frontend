95c6c88aea1a5a0930ed1e5b22d716bb45c87007
simoneburrai <burrai.s98@gmail.com>
2026-01-20 17:26:45 +0100

    fix: changed baase url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
 
-  baseURL: "http://localhost:3000", 
+  baseURL: "http://127.0.0.1:3000", 
 })
 
 apiClient.interceptors.request.use((config) => {
